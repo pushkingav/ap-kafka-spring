f0c4b21957c80f54714537575ae5a45a1c07223c
Andrei Pushkin1 <Andrei_Pushkin1@epamyerw02ed.epam.com>
2023-05-16 13:28:55 +0400

    Initial commit

---
+++ src/main/java/com/apushking/apkafkaspring/ApKafkaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.apushking.apkafkaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApKafkaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApKafkaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apushking/apkafkaspring/ApKafkaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apushking.apkafkaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApKafkaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
